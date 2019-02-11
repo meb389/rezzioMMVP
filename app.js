7aec0a03bba3da7efdff59008ca88ff06f986ec2
Mitch Brudy <mitchbrudel@gmail.com>
2019-02-11 17:27:25 -0500

    Testing

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const express         = require("express"),
       methodOverride  = require("method-override"),
       expressSanitzer = require("express-sanitizer");
 
+      app.use(bodyParser.urlencoded({extended: true}));
+      app.use(expressSanitzer());
+      app.use(express.static("public"));
+      app.use(methodOverride("_method"));
+      app.set("view engine", "ejs");
 
+app.get("/", function(req, res){
+  res.send("Home Page");
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started.");
