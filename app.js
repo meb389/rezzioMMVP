1b3e07c4cc225b71c095b2e0841896acb2711e61
Mitch Brudy <mitchbrudel@gmail.com>
2019-02-11 17:08:32 -0500

    Node Setup
    Node Set up

---
+++ app.js
@@ -0,0 +1,10 @@
+const express         = require("express"),
+      app             = express(),
+      bodyParser      = require("body-parser"),
+      methodOverride  = require("method-override"),
+      expressSanitzer = require("express-sanitizer");
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started.");
+});
