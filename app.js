f33b3e3246f6f0b5f670b002e45795b28f1bfac4
Mitch Brudy <mitchbrudel@gmail.com>
2019-02-16 15:59:45 -0500

    Minor change to createUser

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const expressSanitzer = require("express-sanitizer"),
       bodyParser      = require("body-parser"),
       express         = require('express'),
       mongoose        = require('mongoose'),
-      userAccount     = require('./models/Schema/user');
-      app = express(),
-
+      User            = require('./models/Schema/user');
       //rezzio
        //mongoose.connect(`mongodb://Amadou:AmadouPassword@cluster0-shard-00-00-lujlt.mongodb.net:27017,cluster0-shard-00-01-lujlt.mongodb.net:27017,cluster0-shard-00-02-lujlt.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`, { useNewUrlParser: true })
        // mine
@@ -13,73 +11,71 @@ const expressSanitzer = require("express-sanitizer"),
         .then(() => console.log('Connected'))
         .catch(err => console.log(err));
 
+      app = express(),
       app.use(bodyParser.urlencoded({extended: true}));
       app.use(expressSanitzer());
       app.use(express.static("public"));
       app.use(methodOverride("_method"));
       app.set("view engine", "ejs");
 
-      // Load user Schema
-      const User = require('./models/Schema/user');
+      // Index
+      app.get('/', (req, res) => res.render('index'));
+      // Open about-you page for new users
+      app.get('/about-you', (req, res) => res.render('createUser'));
+      // Open careerPath page for new users
+      app.get('/careerpath', (req, res) => res.render('createCareerPath'));
+      //Questionnaire
+      app.get('/questionnare', (req, res) => res.render('questionnare'));
+      // Open awareness page for new users
+      app.get('/awareness', (req, res) => res.render('createAwareness'));
+      // Open careerAwareness page for new users
+      app.get('/careerawareness', (req, res) => res.render('createCareerAwareness'));
+      // Open mentorship page for new users
+      app.get('/mentorship', (req, res) => res.render('CreateMentorship'));
+      // Open exposure page for new users
+      app.get('/exposure', (req, res) => res.render('CreateExposure'));
+      // Open internship page for new users
+      app.get('/internship', (req, res) => res.render('createInternship'));
+      // Open networkingA page for new users
+      app.get('/networking', (req, res) => res.render('createNetworking'));
+      // Open involvementA page for new users
+      app.get('/involvement', (req, res) => res.render('createInvolvement'));
+      // Thank You page
+      app.get('/thankyou', (req, res) => res.render('thankYou'));
 
-// Index
-app.get('/', (req, res) => res.render('index'));
-// Open about-you page for new users
-app.get('/about-you', (req, res) => res.render('createUser'));
 // Post route for submiting user data
 app.post('/about-you', (req, res) => {
   // Get data from form and add to users profile
-  var firstName = req.body.aa;
-  var lastName = req.body.ab;
-  var gender = req.body.ac;
-  var emailAddress = req.body.ad;
-  var currentMajor = req.body.ae;
-  var currentMinor = req.body.af;
-  var currentGrade = req.body.ag;
-  var graduationDate = req.body.ah;
-  var newUser = {
-    firstName: firstName,
-    lastName: lastName,
-    gender: gender,
-    emailAddress: emailAddress,
-    currentMajor: currentMajor,
-    currentMinor: currentMinor,
-    currentGrade: currentGrade,
-    graduationDate: graduationDate
-    };
+  let firstName = req.body.aa,
+      lastName = req.body.ab,
+      gender = req.body.ac,
+      emailAddress = req.body.ad,
+      currentMajor = req.body.ae,
+      currentMinor = req.body.af,
+      currentGrade = req.body.ag,
+      graduationDate = req.body.ah;
+
+  let newUser = {
+      firstName: firstName,
+      lastName: lastName,
+      gender: gender,
+      emailAddress: emailAddress,
+      currentMajor: currentMajor,
+      currentMinor: currentMinor,
+      currentGrade: currentGrade,
+      graduationDate: graduationDate
+  };
+
   // Create a new User profile and save to DB
   User.create(newUser, function(err, newlyCreated){
-      if(err){
-        console.log(err);
-      } else{
-        // Redirect to next page
-        res.render('createCareerPath');
-      }
+    if(err){
+      console.log(err);
+    } else{
+      // Redirect to next page
+      res.render('createCareerPath');
+    }
   });
 });
-// Open careerPath page for new users
-app.get('/careerpath', (req, res) => res.render('createCareerPath'));
-//Questionnaire
-app.get('/questionnare', (req, res) => res.render('questionnare'));
-// Open awareness page for new users
-app.get('/awareness', (req, res) => res.render('createAwareness'));
-// Open careerAwareness page for new users
-app.get('/careerawareness', (req, res) => res.render('createCareerAwareness'));
-// Open mentorship page for new users
-app.get('/mentorship', (req, res) => res.render('CreateMentorship'));
-// Open exposure page for new users
-app.get('/exposure', (req, res) => res.render('CreateExposure'));
-// Open internship page for new users
-app.get('/internship', (req, res) => res.render('createInternship'));
-// Open networkingA page for new users
-app.get('/networking', (req, res) => res.render('createNetworking'));
-// Open involvementA page for new users
-app.get('/involvement', (req, res) => res.render('createInvolvement'));
-
-// Thank You page
-app.get('/thankyou', (req, res) => res.render('thankYou'));
-
-
 
 //CREATE New User object
 // app.post('/about-you', (req, res) => {
@@ -112,25 +108,6 @@ app.get('/thankyou', (req, res) => res.render('thankYou'));
 //       }
 //     });
 // });
-
-app.post('/awareness', (req, res) => {
-  // req.send('hello');
-  console.log(req.body);
-  //res.send('hello');
-  //let {} = req.body;
-
-  // let newUser = {
-  //
-  // };
-  //   // Create user and save to DB
-  //   User.create(newUser, (err, createdUser) => {
-  //     if(err){
-  //       console.log(err);
-  //     } else {
-  //       res.redirect('awareness');
-  //     }
-  //   });
-});
 
 
 app.listen(5000, () => console.log('Example app listening on port 5000!'))
