1e26f0201a5110bff0acc6c3ad888841dbd652f5
Mitch Brudy <mitchbrudel@gmail.com>
2019-02-18 15:17:05 -0500

    Testing Exposure hidden data entry

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,3 +1,5 @@
+let yesNo = "Yes";
+
 $('.ui.dropdown')
   .dropdown()
 ;
@@ -5,9 +7,12 @@ $('.ui.dropdown')
 $("#hideSelectorYes").on("click", function(){
   $("#introQuestion").css("display", "none");
   $("#mainQuestions").css("display", "block");
+  $("#yesNoStorage").text(yesNo);
 });
 
 $("#hideSelectorNo").on("click", function(){
+  yesNo = "No";
   $("#introQuestion").css("display", "none");
   $("#mainQuestions").css("display", "block");
+  $("#yesNoStorage").text(yesNo);
 });
