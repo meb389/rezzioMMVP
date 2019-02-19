b14ae2dbd932849f627ee1ebb1d98794e7161913
Mitch Brudy <mitchbrudel@gmail.com>
2019-02-19 16:20:06 -0500

    Testing awareness and Login exmbed

--- models/Schema/user.js
+++ models/Schema/user.js
@@ -1,14 +1,26 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-   firstName: String,
-   lastName: String,
-   gender: String,
-   emailAddress: String,
-   currentMajor: String,
-   currentMinor: String,
-   currentGrade: String,
-   graduationDate: String,
+  firstName: String,
+  lastName: String,
+  gender: String,
+  emailAddress: String,
+  currentMajor: String,
+  currentMinor: String,
+  currentGrade: String,
+  graduationDate: String,
+  login: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Login"
+    }
+  ],
+  awareness: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Awareness"
+    }
+  ],
 });
 
 const User = mongoose.model("User", userSchema);
