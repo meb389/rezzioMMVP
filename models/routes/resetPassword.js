768b269960c6a2ff219b815fd254ecf38f5c0cbd
Amadou Diallo <ad3464@drexel.edu>
2019-02-25 12:48:10 -0500

    implemented the password reset system.

--- models/routes/resetPassword.js
+++ models/routes/resetPassword.js
@@ -1,5 +1,6 @@
 const express    = require("express"),
       passport   = require("passport"),
+      mongoose        = require("mongoose"),
       router     = express.Router(),
       User       = require("../Schema/user"),
       Networking = require("../Schema/networking")
@@ -8,27 +9,50 @@ const express    = require("express"),
 router.route("/resetPassword")
   .get((req, res) => res.render("resetPassword"))
   .post((req, res) => {
-    const { la, lb, lc, ld, yesNo } = req.body
-    const newNetworking = {
-          Nquestion1: la,
-          Nquestion2: lb,
-          Nquestion3: lc,
-          Nquestion4: ld,
-          Nquestion5: yesNo
-    }
+    console.log(req.body);
+    // users.findOne({ username: `${req.body.uId}` })
+    //   .then((err, result) => {console.log(result)});
+    // User.find({username: 'test2'}, function(err,obj) { console.log(obj[0].password); });
+    const {uId, newPassword} = req.body;
+    User.findByUsername(uId).then(function(sanitizedUser){
+      console.log(sanitizedUser);
+        if (sanitizedUser){
+            sanitizedUser.setPassword(newPassword, function(){
+                sanitizedUser.save();
+                res.status(200).json({message: 'password reset successful'});
+            });
+        } else {
+            res.status(500).json({message: 'This user does not exist'});
+        }
+    },function(err){
+        console.error(err);
+    })
+
+
+
+
+
+    // const { la, lb, lc, ld, yesNo } = req.body
+    // const newNetworking = {
+    //       Nquestion1: la,
+    //       Nquestion2: lb,
+    //       Nquestion3: lc,
+    //       Nquestion4: ld,
+    //       Nquestion5: yesNo
+    // }
 
   // Create a new User profile and save to DB
-  Networking.create(newNetworking, (err, createdNetworking) => {
-    if(err){
-      console.log(err)
-    } else{
-      createdNetworking.currentUser.id = req.user._id
-      createdNetworking.currentUser.username = req.user.username
-      createdNetworking.save()
-      // Redirect to next page
-      res.render("createInvolvement")
-    }
-  })
+  // Networking.create(newNetworking, (err, createdNetworking) => {
+  //   if(err){
+  //     console.log(err)
+  //   } else{
+  //     createdNetworking.currentUser.id = req.user._id
+  //     createdNetworking.currentUser.username = req.user.username
+  //     createdNetworking.save()
+  //     // Redirect to next page
+  //     res.render("createInvolvement")
+  //   }
+  // })
 })
 
 // Function to chech if loggedIn
